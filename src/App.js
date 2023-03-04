9a1c77ee609878bfa114add31d29dd1592e3e1f6
vincent <chiwang1712@gmail.com>
2023-03-04 11:48:37 +0800

    change to react native

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn
+          Learn React native
         </a>
       </header>
     </div>
