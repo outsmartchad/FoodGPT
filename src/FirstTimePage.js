08c9311b12ce2c5cf346c2b1ebeb6bbbec61d9ee
Alizan12378 <126301538+Alizan12378@users.noreply.github.com>
2023-03-11 10:21:31 +0800

    Add files via upload

--- src/FirstTimePage.js
+++ src/FirstTimePage.js
@@ -1,23 +1,23 @@
 import React from "react";
-import HomePage from "./Homepage";
 import { Link } from "react-router-dom";
-import Preference from "./Preference";
 
 export default function FirstTimePage() {
   return (
     <div>
       <div>
-        <p>First time?</p>
-        {/* to create a famous meme */}
-        <img
-          src={process.env.PUBLIC_URL + "/first-time-james-franco.gif"}
-          alt="First Time?"
-        />
-        <br></br>
-        <Link to="/Preference">係呀...唔好整咁多色水</Link>
-        <br></br>
-        <br></br>
-        <Link to="/Homepage">關你咩事呢毒撚</Link>
+        <div class="card">
+         <div class="header">
+            <h1>First time?</h1>
+         </div>
+
+          <div class="container">
+             <p>
+             <Link to="/Preference" className="YesUI">係呀</Link>
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <Link to="/Homepage" className="NoUI">唔係</Link>
+             </p>
+           </div>
+        </div>
       </div>
     </div>
   );
