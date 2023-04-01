019e51530c706e6b78ab7bd3dd0979d6b2f10897
Arvin9200 <126305694+Arvin9200@users.noreply.github.com>
2023-04-02 00:36:10 +0800

    Update Personal.js

--- src/Personal.js
+++ src/Personal.js
@@ -1,81 +1,204 @@
-import { Link } from "react-router-dom";
 import { useState } from "react";
 
 export default function Personal() {
-  const [showMenu1, setShowMenu1] = useState(false);
-  const [showMenu2, setShowMenu2] = useState(false);
-  const [showMenu3, setShowMenu3] = useState(false);
-  const [showMenu4, setShowMenu4] = useState(false);
+  const [userData, setUserData] = useState({
+    name: "So Chi Wang",
+    email: "Sogo@example.com",
+    age: 20,
+    address: "35 BARKER ROAD"
+  });
 
-  const handleMenuClick = (menuNumber) => {
-    switch (menuNumber) {
-      case 1:
-        setShowMenu1(!showMenu1);
-        break;
-      case 2:
-        setShowMenu2(!showMenu2);
-        break;
-      case 3:
-        setShowMenu3(!showMenu3);
-        break;
-      case 4:
-        setShowMenu4(!showMenu4);
-        break;
-      default:
-        break;
-    }
+  const [isEditing, setIsEditing] = useState(false);
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
+    setIsEditing(false);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUserData((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
   };
 
   return (
-    <div>
-      <div className="header2">
+    <div className="personal-container">
+      <div className="header">
         <h1>個人資料</h1>
       </div>
-      <div className="container2">
-      <button onClick={() => handleMenuClick(1)}>Menu 1</button>
-      {showMenu1 && (
-        <ul>
-          <li>Option 1</li>
-          <li>Option 2</li>
-          <li>Option 3</li>
-        </ul>
-      )}
+      <div className="content-container">
+        <div className="info">
+          <p>
+            <strong>姓名：</strong>
+            {isEditing ? (
+              <input
+                type="text"
+                name="name"
+                value={userData.name}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <span>{userData.name}</span>
+            )}
+          </p>
+          <p>
+            <strong>電子郵件：</strong>
+            {isEditing ? (
+              <input
+                type="email"
+                name="email"
+                value={userData.email}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <span>{userData.email}</span>
+            )}
+          </p>
+          <p>
+            <strong>年齡：</strong>
+            {isEditing ? (
+              <input
+                type="number"
+                name="age"
+                value={userData.age}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <span>{userData.age}</span>
+            )}
+          </p>
+          <p>
+            <strong>地址：</strong>
+            {isEditing ? (
+              <input
+                type="text"
+                name="address"
+                value={userData.address}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <span>{userData.address}</span>
+            )}
+          </p>
+        </div>
+        <div className="button-container">
+          {isEditing ? (
+            <div className="buttons">
+              <button className="save-btn" onClick={handleSave}>
+                保存
+              </button>
+              <button className="cancel-btn" onClick={handleCancel}>
+                取消
+              </button>
+            </div>
+          ) : (
+            <div className="buttons">
+              <button className="edit-btn" onClick={handleEdit}>
+                編輯
+              </button>
+            </div>
+          )}
+        </div>
       </div>
-      <div className="container2">
-      <button onClick={() => handleMenuClick(2)}>Menu 2</button>
-      {showMenu2 && (
-        <ul>
-          <li>Option 4</li>
-          <li>Option 5</li>
-          <li>Option 6</li>
-        </ul>
-      )}
-      </div>
-      <div className="container2">
-      <button onClick={() => handleMenuClick(3)}>Menu 3</button>
-      {showMenu3 && (
-        <ul>
-          <li>Option 7</li>
-          <li>Option 8</li>
-          <li>Option 9</li>
-        </ul>
-      )}
-      </div>
-      <div className="container2">
-      <button onClick={() => handleMenuClick(4)}>Menu 4</button>
-      {showMenu4 && (
-        <ul>
-          <li>Option 10</li>
-          <li>Option 11</li>
-          <li>Option 12</li>
-        </ul>
-      )}
-      </div>
-      <div className="header">
-      <Link to="/HomePage">
-        <div className="backToHomeBtn">↩️ 返去主頁面</div>
-      </Link>
-      </div>
-    </div>
-  );
+      <style jsx>{`
+        .personal-container {
+          background-image: url("/background-image.jpg");
+          background-size: cover;
+          background-position: center;
+          color: #000000;
+          font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+        }
+
+        .header {
+          padding: 2rem;
+          background: linear-gradient(to bottom right, #00FF00, #00FF00);
+          text-align: center;
+          font-size: 
+16px;
+font-weight: bold;
+}
+.content-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin-top: 3rem;
+}
+
+.info {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+p {
+  margin-bottom: 1.5rem;
+  font-size: 1.2rem;
+}
+
+strong {
+  font-weight: bold;
+}
+
+input[type="text"],
+input[type="email"],
+input[type="number"] {
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+  width: 100%;
+}
+
+input[type="text"]:focus,
+input[type="email"]:focus,
+input[type="number"]:focus {
+  outline: none;
+  border: 2px solid #00FF00;
+}
+
+.buttons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1.5rem;
+}
+
+.edit-btn,
+.save-btn {
+  padding: 0.5rem 1rem;
+  margin-right: 1rem;
+  background-color: #00FF00;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+}
+
+.edit-btn:hover,
+.save-btn:hover {
+  background-color: #006666;
+}
+
+.cancel-btn {
+  padding: 0.5rem 1rem;
+  background-color: #00FF00;
+  color: #fff;
+  border: 2px solid #00FF00;
+  cursor: pointer;
+}
+
+.cancel-btn:hover {
+  background-color: #006666;
+  color: #fff;
+}
+`}</style>
+</div>
+);
 }
