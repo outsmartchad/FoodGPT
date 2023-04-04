a671077310c583838bd414fceeafb189dd667262
Arvin9200 <126305694+Arvin9200@users.noreply.github.com>
2023-04-05 00:11:39 +0800

    Update Homepage.js

--- src/Homepage.js
+++ src/Homepage.js
@@ -13,6 +13,7 @@ export default function HomePage() {
     const localFavorites = localStorage.getItem("favorites");
     return localFavorites ? JSON.parse(localFavorites) : [];
   });
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     fetch("https://manofdiligence.github.io/Restaurant.json")
@@ -48,10 +49,7 @@ export default function HomePage() {
   return (
     <div>
       <div>
-        {/* created a dropdown and title it by user choice */}
         <DropdownButton title={selectedOption} onSelect={handleSelectChange}>
-          {/* to get each element in the area array 
-          and put it as a dropdown item component */}
           {area.map((item, index) => (
             <Dropdown.Item key={index} eventKey={item}>
               {item}
@@ -60,18 +58,16 @@ export default function HomePage() {
         </DropdownButton>
       </div>
       <div className="header">
-        {/* Search Bar class */}
-
-        <Link to="/AdvancedSearch">
-          <img
-            src={process.env.PUBLIC_URL + "./search.jpeg"}
-            alt="search Bar"
-            height="100px"
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="搜索餐廳"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-        </Link>
+        </div>
       </div>
       <div className="container2">
-        {/* near restaurant, restaurant rank*/}
         <p>
           <Link to="/NearRestaurant">
             <img
@@ -86,34 +82,92 @@ export default function HomePage() {
           </Link>
         </p>
       </div>
-      {/* restaurant info class */}
       <div>
         <h2>餐廳資訊</h2>
       </div>
       <div>
-      {/* Other components and elements */}
-      {data.map((item, index) => {
-        const isFavorite = favorites.some((fav) => fav.id === item.id);
-        return (
-          <div key={index} id={item.id} className="container2">
-            <h2>{item.Name}</h2>
-            <img src={item.Image} alt="rest photo" width="300px" />
-            <p>{item.Area}</p>
-            <button onClick={() => toggleFavorite(item)}>
-              {isFavorite ? "⭐️" : "★"}
-            </button>
-          </div>
-        );
-      })}
-    </div>
-    <style jsx>{`
+        {data
+          .filter((item) =>
+            item.Name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .map((item, index) => {
+            const isFavorite = favorites.some((fav) => fav.id === item.id);
+            return (
+              <div key={index} id={item.id} className="container2">
+                <h2>{item.Name}</h2>
+                <img src={item.Image} alt="rest photo" width="300px" />
+                <p>{item.Area}</p>
+                <button onClick={() => toggleFavorite(item)}>
+                  {isFavorite ? "⭐️" : "★"}
+                </button>
+              </div>
+            );
+          })}
+      </div>
+      <style jsx>{`
         body {
           font-family: Arial, sans-serif;
           margin: 0;
           padding: 20px;
-          background-image: linear-gradient(to right, #FF8EFF, 	#84C1FF);
+          background-image: linear-gradient(to right, #FF8EFF, #84C1FF);
         }
-        `}</style>
+
+        .header {
+          display: flex;
+          justify-content: center;
+          margin: 20px 0;
+        }
+
+        .search-bar {
+          width: 50%;
+          position: relative;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+
+        .search-bar input {
+          width: 100%;
+          padding: 10px;
+          font-size: 18px;
+          border: 1px solid #ccc;
+          border-radius: 5px;
+          box-sizing: border-box;
+        }
+        .container2 {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: space-around;
+          background-color: #fff;
+          border-radius: 8px;
+          margin: 10px 0;
+          padding: 20px;
+        }
+
+        h2 {
+          margin: 0;
+        }
+
+        p {
+          margin: 0;
+        }
+
+        img {
+          border-radius: 8px;
+        }
+
+        button {
+          background-color: transparent;
+          border: none;
+          font-size: 24px;
+          cursor: pointer;
+        }
+
+        button:focus {
+          outline: none;
+        }
+      `}</style>
     </div>
   );
 }
