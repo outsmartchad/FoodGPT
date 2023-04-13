d49ae330b9bf85649f6161977f893f4883d87e0b
Alizan12378 <126301538+Alizan12378@users.noreply.github.com>
2023-04-13 23:12:21 +0800

    Add files via upload

--- src/FirstTimePage.js
+++ src/FirstTimePage.js
@@ -29,12 +29,14 @@ export default function FirstTimePage() {
         <div className="container2">
           <h1>--FoodGPT-- 項目信息:</h1>
         </div>
-        <div className="container3">
+        <div className="container2">
           <p>
             FoodGPT是一種搜尋HKCC附近食品的工具，考慮到忙碌的 HKCC 學生的需求。
             它提供了當地餐館和食品店的綜合數據庫，包括有關菜單項目、價格等的詳細信息。此外，該應用程序具有多種過濾器和搜索選項，允許用戶根據他們的特定偏好和要求定制他們的食物搜索選項，使他們能夠輕鬆準確地找到正在尋找的東西。是尋求快速簡便地尋找美食的
             HKCC 學生的理想解決方案。
           </p>
+        </div>
+        <div className="container3">
           <p>**FoodGPT 向所有用戶免費開放</p>
         </div>
       </div>
