92610b4e6722b8072981dc108f74109d044aef18
chiwangso <chiwang1712@gmail.com>
2023-04-13 21:59:42 +0800

    get current area and gen rest

--- src/NearRestaurant.js
+++ src/NearRestaurant.js
@@ -1,95 +1,76 @@
-import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
-import Geocode from "react-geocode";
+import React, { useState, useEffect } from "react";
 
-// Set your Google Maps API key here
+export default function NearRestaurants() {
+  const [district, setDistrict] = useState("");
+  const [restaurants, setRestaurants] = useState([]);
+  const [map, setMap] = useState(null);
 
-export default function NearRestaurant() {
-  const [areaName, setAreaName] = useState("");
-  const [mapUrl, setMapUrl] = useState("");
+  //locate the district
+  useEffect(() => {
+    const getLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(getPosition);
+      } else {
+        console.error("Geolocation is not supported by this browser.");
+      }
+    };
 
-  function displayNearbyRestaurants(lat, lng) {
-    // Construct a Google Maps API url
-    const mapUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=15&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C${lat},${lng}&key=AIzaSyCkPkl9UPI2puzUtW3qOpVjmOQywu0YIwk`;
-
-    // Update state variables
-    setMapUrl(mapUrl);
-
-    // Fetch the address from the Google Maps Geocoding API
-    fetch(
-      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyCkPkl9UPI2puzUtW3qOpVjmOQywu0YIwk`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        const addressComponents = data.results[0].address_components;
-
-        let areaName = "";
-        for (let i = 0; i < addressComponents.length; i++) {
-          const component = addressComponents[i];
-          if (
-            component.types.includes("neighborhood") ||
-            component.types.includes("sublocality")
-          ) {
-            areaName = component.long_name;
-            break;
+    const getPosition = (position) => {
+      const { latitude, longitude } = position.coords;
+      fetch(
+        `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json&addressdetails=1`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          const address = data.address;
+          let districtName = "";
+          const districtKeys = ["district", "borough", "city_district"];
+          for (let i = 0; i < districtKeys.length; i++) {
+            if (address[districtKeys[i]]) {
+              districtName = address[districtKeys[i]];
+              break;
+            }
           }
-        }
+          if (districtName) {
+            console.log(`District: ${districtName}`);
+            setDistrict(districtName);
+          } else {
+            console.error("Unable to determine district from address.");
+          }
+        })
+        .catch((error) => console.error(error));
+    };
 
-        setAreaName(areaName);
+    getLocation();
+  }, []);
+
+  //fetch our restaurant database
+  useEffect(() => {
+    if (!district) return;
+    fetch("https://manofdiligence.github.io/Restaurant.json")
+      .then((response) => response.json())
+      .then((result) => {
+        setMap(result);
+        let districtRestaurants =
+          result && result[district] ? result[district] : [];
+        setRestaurants(districtRestaurants);
       })
-      .catch((error) => console.error(error));
-  }
-
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          console.log(position);
-          const lat = position.coords.latitude;
-          const lng = position.coords.longitude;
-          displayNearbyRestaurants(lat, lng);
-        },
-        (error) => {
-          console.error(error);
-        }
-      );
-    } else {
-      console.error("Geolocation is not supported by this browser.");
-    }
-  }
+      .catch((err) => console.error(err));
+  }, [district]);
 
   return (
-    <div>
-      <div className="head">
-        <h1>附近餐廳</h1>
-      </div>
-
-      <div>
-        <button onClick={getLocation}>Get Current Location</button>
-        <div>{areaName}</div> {/* Display the area name here */}
-        <div>
-          {mapUrl && <img src={mapUrl} alt="Map" />}{" "}
-          {/* Display the map here */}
+    <div className="rankingList">
+      <h2>{district ? `你所在嘅地址為： ${district}` : "Loading..."}</h2>
+      {restaurants.map((item, index) => (
+        <div key={index} className="container2">
+          <h2>{item.name}</h2>
+          <h2>{item.type}</h2>
+          <img src={item.image} alt="rest" width="300px" />
+          <h2>{item.Popularity}</h2>
+          <h2>{item.district}</h2>
+          <h2>{item.address}</h2>
         </div>
-      </div>
-
-      <div className="header">
-        <Link to="/HomePage">
-          <div className="backToHomeBtn">↩️ 返去主頁面</div>
-        </Link>
-      </div>
-      <style jsx={true}>{`
-        .head {
-          background-image: linear-gradient(to right, #ff8eff, #84c1ff);
-        }
-        body {
-          font-family: Arial, sans-serif;
-          margin: 0;
-          padding: 20px;
-          background-image: linear-gradient(to right, #ff8eff, #84c1ff);
-        }
-      `}</style>
+      ))}
     </div>
   );
 }
