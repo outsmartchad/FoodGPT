8029c028b45ab9918d0e639bfbfce2164fa1ee5a
chiwangso <chiwang1712@gmail.com>
2023-04-20 10:50:34 +0800

    updated intro

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,6 +1,7 @@
 import { useLocation, Link } from "react-router-dom";
 import React, { useState, useEffect, useRef } from "react";
 import "leaflet/dist/leaflet.css";
+import { LoadScript } from "@react-google-maps/api";
 import {
   GoogleMap,
   Marker,
@@ -16,10 +17,27 @@ function Map() {
 
   return { isLoaded, loadError };
 }
+
 export default function Restaurant() {
   const [restaurantData, setRestaurantData] = useState(null);
-
   const location = useLocation();
+  const initialRestaurantData = location.state && location.state.Restaurant;
+  const [coords, setCoords] = useState(
+    initialRestaurantData
+      ? { lat: initialRestaurantData.lat, lng: initialRestaurantData.lng }
+      : { lat: 0.0, lng: 0.0 }
+  );
+  // ...
+  // x and y are the coordinates of the map center
+  const initialCoords = {
+    lat: coords.lat,
+    lng: coords.lng,
+  };
+
+  const libraries = ["places"];
+
+  const [markerPosition, setMarkerPosition] = useState(initialCoords);
+  const [searchInput, setSearchInput] = useState("");
 
   const libs = ["places"];
   // size of the map
@@ -27,11 +45,7 @@ export default function Restaurant() {
     width: "100%",
     height: "500px",
   };
-  // x and y are the coordinates of the map center
-  const initialCoords = {
-    lat: 40.7128,
-    lng: -74.006,
-  };
+
   // google map
   const { isLoaded, loadError } = Map();
   const mapRef = useRef();
@@ -50,8 +64,28 @@ export default function Restaurant() {
   useEffect(() => {
     if (location.state && location.state.Restaurant) {
       setRestaurantData(location.state.Restaurant);
+      setSearchInput(location.state.Restaurant.address);
+      setCoords({
+        lat: location.state.Restaurant.lat,
+        lng: location.state.Restaurant.lng,
+      });
     }
+    console.log(coords);
   }, [location]);
+  // Add this useEffect to search for the restaurant's address
+  useEffect(() => {
+    if (searchBoxRef.current && mapRef.current && searchInput) {
+      searchBoxRef.current.getPlacesAsync([searchInput]).then((places) => {
+        const firstPlace = places[0];
+        if (firstPlace && firstPlace.geometry) {
+          const { location } = firstPlace.geometry;
+          mapRef.current.panTo(location);
+          mapRef.current.setZoom(15);
+          setMarkerPosition({ lat: location.lat(), lng: location.lng() });
+        }
+      });
+    }
+  }, [searchInput]);
 
   if (loadError) {
     return <div>Error loading map</div>;
@@ -74,19 +108,19 @@ export default function Restaurant() {
           <h3>{restaurantData.address}</h3>
         </div>
         <div className="container2">
-          <h1>{restaurantData.Popularity}</h1>
           <h1>{restaurantData.name}</h1>
           <img src={restaurantData.image} alt="rest photo" width="300px" />
+          <h2>{restaurantData.Intro}</h2>
         </div>
       </div>
       <div className="container2">
         <GoogleMap
           mapContainerStyle={mapContainerStyle}
-          center={initialCoords}
+          center={coords} // Update this line
           zoom={12}
           onLoad={onMapLoad}
         >
-          <Marker position={initialCoords} />
+          <Marker position={markerPosition} />
         </GoogleMap>
       </div>
       <div className="header">
